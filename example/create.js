9e2e70d6afae4252527c3f1c1669809074986249
Tim Caswell <tim@creationix.com>
2013-06-15 11:12:23 -0500

    Fix typo in sample repo code

--- example/create.js
+++ example/create.js
@@ -20,7 +20,7 @@ var commits = {
   "Implement awesome and bump version to 3.1.4": {
     "README.md": "# This is a test Repo\n\nIt's generated entirely by JavaScript\n",
     "package.json": '{\n  "name": "awesome-lib",\n  "version": "3.1.4",\n  "main": "awesome.js"\n}\n',
-    "awesome.js": 'module.exports = function () {\n  return 42;\n'
+    "awesome.js": 'module.exports = function () {\n  return 42; \n};\n'
   }
 };
 
